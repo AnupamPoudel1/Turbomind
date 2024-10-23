a41e8c5611e5ddd688d002eb11ce17e8d8eb12de
Anupam <anupampoudel78@gmail.com>
2024-10-23 15:33:10 +0545

    Modification: minor modification to section header

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,11 +1,13 @@
+import { PropsWithChildren } from "react";
 import { twMerge } from "tailwind-merge";
 
-const SectionHeader = ({ title, titleYellow, description, className }: { title: string; titleYellow: string; description: string; className?: string }) => {
+const SectionHeader = ({
+    children,
+    description,
+    className }: PropsWithChildren<{ description: string; className?: string }>) => {
     return (
         <div className={twMerge("flex flex-col justify-center items-center text-center gap-3", className)}>
-            <h1 className="font-serif text-3xl lg:text-[2.5rem] font-semibold py-1">
-                {title} <span className="text-secondary">{titleYellow}</span>
-            </h1>
+            {children}
             <p className="py-1 text-base text-text font-normal">
                 {description}
             </p>
